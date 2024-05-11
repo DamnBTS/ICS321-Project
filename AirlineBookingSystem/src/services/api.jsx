10b4d754d805e35bfe5f20a418137055027dbd42
Mohammed <mmoohhaammmmeedd133@gmail.com>
2024-05-11 12:37:37 +0300

    removed old commented code

--- AirlineBookingSystem/src/services/api.jsx
+++ AirlineBookingSystem/src/services/api.jsx
@@ -1,21 +1,3 @@
-// import axios from 'axios';
-
-// const API_URL = 'http://localhost:5173/'; // Replace with your API URL
-
-// const api = axios.create({
-//   baseURL: API_URL,
-// });
-
-// export const login = (email, password) => {
-//   return api.post('/login', { email, password });
-// };
-
-// export const getPassengers = () => {
-//   return api.get('/passengers');
-// };
-
-// Add other API methods as needed
-
 import axios from 'axios';
 
 const API_URL = 'http://localhost:5000'; // Replace with your API URL
